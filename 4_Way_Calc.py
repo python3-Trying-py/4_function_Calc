1364ddd2ddebeed3af74359308e7d8b765689a72
python3-Trying-py <46845836+python3-Trying-py@users.noreply.github.com>
2019-01-24 20:29:50 +0000

    6_function_calc.py
    added usable operations list and squaring and exponents to the operations

--- 4_Way_Calc.py
+++ 4_Way_Calc.py
@@ -15,7 +15,11 @@ while True:
     OpM1="*"
     OpD="divide"
     OpD1="/"
-    Var3=input("what operation would you like?\n")
+    Opsq="square"
+    Opsq1="s"
+    OpE="exponent"
+    OpE1="e"
+    Var3=input("what operation would you like? Trying has programmed add(+), subtract(-), multiply(*), divide(/), square(s), and exponent(e)\n")
     if OpA==Var3 or OpA1==Var3:
         print(Var1 + Var2)
     elif OpS==Var3 or OpS1==Var3:
@@ -23,6 +27,10 @@ while True:
     elif OpM==Var3 or OpM1==Var3:
         print(Var1 * Var2)
     elif OpD==Var3 or OpD1==Var3:
-        print(Var1 // Var2)
+        print(Var1 / Var2)
+    elif Opsq==Var3 or Opsq1==Var3:
+        print(Var1 ** Var1)
+    elif OpE==Var3 or OpE1==Var3:
+        print(Var1 ** Var2)
     else:
         print("Invalid Operation")
